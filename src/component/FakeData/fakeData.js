1baa14d158f2e559c930c9a47e4ef9b1ba7c537e
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-07 20:43:08 +0600

    practice tailwind

---
+++ src/component/FakeData/fakeData.js
@@ -0,0 +1,111 @@
+
+  var data = [
+    {
+      "id": "1",
+      "title": "Slack",
+      "img": "https://i.ibb.co/Rj1HByQ/pngaaa-com-3764183.png"
+    },
+    {
+      "id": "2",
+      "title": "Zapier",
+      "img": "https://i.ibb.co/QM9bVpW/integration-icon-zapier-large.png"
+    },
+    {
+      "id": "3",
+      "title": "Github",
+      "img": "https://i.ibb.co/6PFW3Hj/github-073010959a20a0b8a5a6ae3ab5529f76a4f33f73e48b1f5282eeb1a461502dc1.png"
+    },
+    {
+        "id": "4",
+        "title": "Slack",
+        "img": "https://i.ibb.co/Rj1HByQ/pngaaa-com-3764183.png"
+      },
+      {
+        "id": "5",
+        "title": "Zapier",
+        "img": "https://i.ibb.co/QM9bVpW/integration-icon-zapier-large.png"
+      },
+      {
+        "id": "6",
+        "title": "Github",
+        "img": "https://i.ibb.co/6PFW3Hj/github-073010959a20a0b8a5a6ae3ab5529f76a4f33f73e48b1f5282eeb1a461502dc1.png"
+      },
+      {
+        "id": "7",
+        "title": "Slack",
+        "img": "https://i.ibb.co/Rj1HByQ/pngaaa-com-3764183.png"
+      },
+      {
+        "id": "8",
+        "title": "Zapier",
+        "img": "https://i.ibb.co/QM9bVpW/integration-icon-zapier-large.png"
+      },
+      {
+        "id": "9",
+        "title": "Github",
+        "img": "https://i.ibb.co/6PFW3Hj/github-073010959a20a0b8a5a6ae3ab5529f76a4f33f73e48b1f5282eeb1a461502dc1.png"
+      },
+      {
+        "id": "10",
+        "title": "Slack",
+        "img": "https://i.ibb.co/Rj1HByQ/pngaaa-com-3764183.png"
+      },
+      {
+        "id": "11",
+        "title": "Zapier",
+        "img": "https://i.ibb.co/QM9bVpW/integration-icon-zapier-large.png"
+      },
+      {
+        "id": "12",
+        "title": "Github",
+        "img": "https://i.ibb.co/6PFW3Hj/github-073010959a20a0b8a5a6ae3ab5529f76a4f33f73e48b1f5282eeb1a461502dc1.png"
+      },
+      {
+        "id": "13",
+        "title": "Slack",
+        "img": "https://i.ibb.co/Rj1HByQ/pngaaa-com-3764183.png"
+      },
+      {
+        "id": "14",
+        "title": "Zapier",
+        "img": "https://i.ibb.co/QM9bVpW/integration-icon-zapier-large.png"
+      },
+      {
+        "id": "15",
+        "title": "Github",
+        "img": "https://i.ibb.co/6PFW3Hj/github-073010959a20a0b8a5a6ae3ab5529f76a4f33f73e48b1f5282eeb1a461502dc1.png"
+      },
+      {
+        "id": "16",
+        "title": "Slack",
+        "img": "https://i.ibb.co/Rj1HByQ/pngaaa-com-3764183.png"
+      },
+      {
+        "id": "17",
+        "title": "Zapier",
+        "img": "https://i.ibb.co/QM9bVpW/integration-icon-zapier-large.png"
+      },
+      {
+        "id": "18",
+        "title": "Github",
+        "img": "https://i.ibb.co/6PFW3Hj/github-073010959a20a0b8a5a6ae3ab5529f76a4f33f73e48b1f5282eeb1a461502dc1.png"
+      },
+      {
+        "id": "19",
+        "title": "Slack",
+        "img": "https://i.ibb.co/Rj1HByQ/pngaaa-com-3764183.png"
+      },
+      {
+        "id": "20",
+        "title": "Zapier",
+        "img": "https://i.ibb.co/QM9bVpW/integration-icon-zapier-large.png"
+      },
+      {
+        "id": "21",
+        "title": "Github",
+        "img": "https://i.ibb.co/6PFW3Hj/github-073010959a20a0b8a5a6ae3ab5529f76a4f33f73e48b1f5282eeb1a461502dc1.png"
+      }
+  ]
+  
+export default data;
+
---
+++ src/component/Home/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "../FakeData/fakeData.js";
+
+const Home = () => {
+  return (
+    <div class="m-10 max-w-md mx-auto bg-white rounded shadow-md overflow-hidden md:max-w-2x3    ">
+      <div class="md:flex">
+        <div class="md:flex-shrink-0">
+         <h5 class="text-base font-bold text-gray-700 m-5">Create a task</h5>
+        </div>
+        
+        <div class="p-8">
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
